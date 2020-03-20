0c60f7acf33ebcbd33da1a5d4f79268a9d504c1c
github username <github email address>
2020-03-20 16:18:00 -0400

    header complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h1 className='home-header'> 
+          Kayla Petersen
+        </h1>
         <NavBar /> 
         <Home /> 
       </div>
